c13551c8c1dc624f2ef62d90b202e7ce1318649c
Abshee0 <abshee3280@gmail.com>
2025-01-12 02:00:15 +0500

    deploy2

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -4,8 +4,8 @@ export const projects: Project[] = [
   {
     id: 'stockpulse',
     title: 'StockPulse - Inventory Management System',
-    description: 'A fukky functional inventory management system with the best user interface and easy navigation with darkmode. Built with React, TypeScript and TailwindCSS for more user friendly and fully responsive design and Supabase database.',
-    image: '../assets/StockPulse.png',
+    description: 'A fully functional inventory management system with the best user interface and easy navigation with darkmode. Built with React, TypeScript and TailwindCSS for more user friendly and fully responsive design and Supabase database.',
+    image: '../src/assets/StockPulse.png',
     tags: ['React', 'TailwindCSS', 'TypeScript' , 'Supabsase'],
     githubUrl: 'https://github.com/Abshee0/StockPulse',
     demoUrl: 'https://starlit-cascaron-8dd58d.netlify.app/'
@@ -14,7 +14,7 @@ export const projects: Project[] = [
     id: 'medicallaboratory',
     title: 'Medical Laboratory Landing Page',
     description: 'A simple landing page for a medical laboratory. Built with modern React and TailwindCSS with sleek animations for more eye friendly and fully responsive design.',
-    image: '../assets/medicalLab.png',
+    image: '../src/assets/medicalLab.png',
     tags: ['React', 'TailwindCSS', 'TypeScript'],
     githubUrl: 'https://github.com/Abshee0/laboratory.git',
     demoUrl: 'https://abshee0.github.io/laboratory/'
@@ -23,7 +23,7 @@ export const projects: Project[] = [
     id: 'hoobank',
     title: 'Hoobank - Banking Landing Page',
     description: 'A simple landiang page for a bank with a sleek UI. Built with React and TailwindCSS for more eye friendly and fully responsive design.',
-    image: '../assets/HooBank.png',
+    image: '../src/assets/HooBank.png',
     tags: ['React', 'TailwindCSS',],
     githubUrl: 'https://github.com/Abshee0/Hoobank.git',
     demoUrl: 'https://abshee0.github.io/Hoobank/'
@@ -32,7 +32,7 @@ export const projects: Project[] = [
     id: 'pulseflow',
     title: 'PulseFlow - Task Management App',
     description: 'A simple kanban task management app with the best user interface and darkmode for the sleek look. Built with React and TailwindCSS for more eye friendly and fully responsive design.',
-    image: '../assets/Pulseflow.png',
+    image: '../src/assets/Pulseflow.png',
     tags: ['React', 'TailwindCSS'],
     githubUrl: 'https://github.com/Abshee0/PulseFlow.git',
     demoUrl: '/'
